a6ebe34a455df68bf0f0dcf6e34f3f15fec8d5ec
rocknegi <rocknegi53@gmail.com>
2019-08-09 17:35:06 +0530

    add image resizer and create a new page for entering new contact info

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom/cjs/react-router-dom';
+import ImageInput from './utils/ImageUtils';
 
 export default class CreateContact extends Component {
     render() {
         return (
             <div>
-                create contact
+               <Link
+               className='close-create-contact'
+               to='/'
+               >
+                   close
+               </Link>
+               <form className='create-contact-avatar-input'>
+                   <ImageInput 
+                   className='create-contact-avatar-input'
+                   name='avatarUrl'
+                   maxHeight={64}
+                   />
+                   <div className='create-contact-details'>
+                    <input type='text name='name placeholder='Name'/>
+                    <input type='text' name ='handle' placeholder='Handle'/>
+                  <button>Add Contact</button>
+                  </div>
+               </form>
             </div>
         )
     }
