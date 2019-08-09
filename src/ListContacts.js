98fc77ccedde8c0840e5abd9a289e2eff46d4921
rocknegi <rocknegi53@gmail.com>
2019-08-09 16:42:41 +0530

    add a form with search bar in a Controlled Component

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,37 +1,75 @@
 import React, { Component } from 'react'
 import propTypes from 'prop-types'
 
- class ListContacts extends Component {
-    render() {
-        return (
-            <ol className='contact-list'>
-                {this.props.contacts.map(contacts => (
-                    <li key={contacts.id} className='contact-list-item' >
-                        <div
-                            className='contact-avatar'
-                            style={{
-                                backgroundImage: `url(${contacts.avatarURL})`
-                            }}
-                        >
 
+class ListContacts extends Component {
+
+    state = {
+        querry: ''
+    }
+    search = (data) => {
+        this.setState({ querry: data.trim() })
+    }
+    clearQuery = ()=>{
+        this.search('')
+    }
+    render() {
+        const { querry } = this.state
+        const { contacts, onDelete } = this.props
+
+        const showingContacts = querry === ''
+            ? contacts
+            : contacts.filter(e => (
+                e.name.toLowerCase().includes(querry.toLowerCase())
+            ))
+
+        return (
+            <div className='list-contacts'>
+                <div className='list-contacts-top'>
+                    <input
+                        className='search-contacts'
+                        type='text'
+                        placeholder='Search Contacts'
+                        value={querry}
+                        onChange={(event) => this.search(event.target.value)}
+                    />
+                </div>
+                {showingContacts.length !== contacts.length && (
+                    <div className='showing-contacts'>
+                        showing {showingContacts.length} of {contacts.length} contacts 
+                        <button onClick={this.clearQuery}>Clear All</button>
                         </div>
-                        <div className='contact-details'>
-                            <p>{contacts.name}</p>
-                            <p>{contacts.handle}</p>
-                        </div>
-                        <button className='contact-remove' onClick={()=>this.props.onDelete(contacts)}>
-                            Remove
+                )}
+                <ol className='contact-list'>
+                    {showingContacts.map(contacts => (
+                        <li key={contacts.id} className='contact-list-item' >
+                            <div
+                                className='contact-avatar'
+                                style={{
+                                    backgroundImage: `url(${contacts.avatarURL})`
+                                }}
+                            >
+
+                            </div>
+                            <div className='contact-details'>
+                                <p>{contacts.name}</p>
+                                <p>{contacts.handle}</p>
+                            </div>
+                            <button className='contact-remove' onClick={() => onDelete(contacts)}>
+                                Remove
                         </button>
-                    </li>
-                ))}
-            </ol>
+                        </li>
+                    ))}
+                </ol>
+
+            </div>
         )
     }
 }
 
-ListContacts.propTypes ={
-    contacts :propTypes.array.isRequired,
-    onDelete : propTypes.func.isRequired
-  }
+ListContacts.propTypes = {
+    contacts: propTypes.array.isRequired,
+    onDelete: propTypes.func.isRequired
+}
 
-  export default ListContacts
+export default ListContacts
