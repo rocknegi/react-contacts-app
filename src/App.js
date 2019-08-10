8ef81dcd40cb6b2466fb8bc4b59555a27d7763ce
rocknegi <rocknegi53@gmail.com>
2019-08-10 21:36:32 +0530

    Add the ability to add a new contact info save the contact to the backend server send you back to the homescreen after sending the data

--- src/App.js
+++ src/App.js
@@ -20,15 +20,30 @@ class App extends Component {
     }))
     ContactsAPI.remove(contact)
   }
+  createContact = (contact) => {
+    ContactsAPI.create(contact)
+      .then(contact => {
+        this.setState((currentState) => ({
+          contacts: currentState.contacts.concat(contact)
+        }))
+      })
+  }
   render() {
     return (
       <div>
         <Route exact path='/' render={() => (
           <ListContacts onDelete={this.onDelete} contacts={this.state.contacts} />
         )} />
-        <Route path='/create' 
-          component={CreateContact} 
-         />
+        <Route path='/create' render={({history}) => (
+          <CreateContact
+            onCreateContact={(contact) => {
+              this.createContact(contact)
+              history.push('/')
+            }}
+          />
+        )}
+
+        />
       </div>
     );
   }
--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom/cjs/react-router-dom';
+import { Link } from 'react-router-dom';
 import ImageInput from './utils/ImageUtils';
+import serializeForm from 'form-serialize'
 
 export default class CreateContact extends Component {
+    handleSubmit = (e)=>{
+        e.preventDefault()
+        const values = serializeForm(e.target,
+            {hash : true})
+console.log(values)
+            if(this.props.onCreateContact){
+                this.props.onCreateContact(values)
+            }
+    }
     render() {
         return (
             <div>
@@ -12,14 +22,14 @@ export default class CreateContact extends Component {
                >
                    close
                </Link>
-               <form className='create-contact-avatar-input'>
+               <form className='create-contact-avatar-input' onSubmit={this.handleSubmit}>
                    <ImageInput 
                    className='create-contact-avatar-input'
                    name='avatarUrl'
                    maxHeight={64}
                    />
                    <div className='create-contact-details'>
-                    <input type='text name='name placeholder='Name'/>
+                    <input type='text' name='name' placeholder='Name'/>
                     <input type='text' name ='handle' placeholder='Handle'/>
                   <button>Add Contact</button>
                   </div>
